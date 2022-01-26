23cdf84b451a0b2b3ab945d26b1975d2eb83e726
Jekabs Locmelis <98461104+edvardsirezelis@users.noreply.github.com>
2022-01-26 12:12:03 +0000

    bija kļūda

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import math
 garums=float(input("Ievadi kvadrāta malas garumu lielāku par 5cm: "))
 laukums1=garums*garums
 laukums2=(garums+5)*(garums+5)
-procenti=round(laukums2-laukums1/laukums2*100)
+procenti=round((laukums2-laukums1)/laukums1*100)
 print(f"Jaunais kvadrāts ir par {procenti} procentiem lielāks.")
